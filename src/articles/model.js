51e4de86e7cb5f34f650c5c1a93408f57fc12da8
rama41222 <dinushankanrg@gmail.com>
2020-07-22 05:04:56 +0530

    feat: article schema

---
+++ src/articles/model.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const articleSchema = new Schema({
+  title: {
+    type: String,
+    trim: true,
+    required: true,
+    unique: true
+  },
+  image: {
+    type: String,
+    default: 'https://getuikit.com/v2/docs/images/placeholder_600x400.svg'
+  },
+  content: {
+    type: String,
+  },
+  isFeatured: {
+    type: Boolean,
+    default: false
+  },
+  tags: {
+    type: Array,
+    default: []
+  },
+  topicId: {
+    type: String,
+    ref: 'Topic',
+    required: true
+  },
+  deleted: {
+    type: Boolean,
+    default: false
+  },
+  views: {
+    type: Number,
+    default: 0
+  },
+  user: {
+    type: String,
+    ref: 'User',
+    required: true,
+  }
+}, { strict: true, timestamps: true });
+
+module.exports = mongoose.model('Topic', articleSchema);
